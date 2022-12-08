ee19f28059a6890e18dd719858e68b1cd712d277
Shruti Gatkal <shrutigatkal2018@gmail.com>
2022-12-08 18:16:27 +0530

    inverted half pyramid complete

--- Invert_half_pyramid.java
+++ Invert_half_pyramid.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+class Invert_half_pyramid{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no.of lines:");
+        int  n=sc.nextInt();
+        sc.close();
+        for (int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
